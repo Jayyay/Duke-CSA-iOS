1931a7bcccd0e229dc58cbaf7ce1a8fd99d807b0
Bill Yu <han.yu@duke.edu>
2016-07-14 20:41:56 -0400

    cloud code for most questioner and answerer

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,6 @@
 Parse.Cloud.define("push", function (request, response) {
     var query;
     var toUser = request.params.toUser; // this is the id of target user
-    console.log(request.user);
     if (request.user && request.user.id == toUser) {
         return;
     }
@@ -19,10 +18,44 @@ Parse.Cloud.define("push", function (request, response) {
     }, {
         useMasterKey: true,
         success: function () {
-            response.success("Success!");
+            response.success("Successfully pushed notification.");
         },
         error: function (error) {
             response.error("Error! " + error.message);
+        }
+    });
+});
+
+Parse.Cloud.define("mostPosts", function (request, response) {
+    var posters = {};
+    var type = request.params.type;
+    var query = new Parse.Query("QAPost");
+    query.find({
+        success: function(results) {
+            console.log("Successfully retrieved " + results.length + " posts.");
+            // Do something with the returned Parse.Object values
+            for (var i = 0; i < results.length; i++) {
+                var post = results[i];
+                if (post.get("type") != type) continue;
+                var authorID = post.get("author").id;
+                if (!posters[authorID]) posters[authorID] = 0;
+                posters[authorID]++;
+            }
+            var max = 0, most = "hwAxx0IwM7";
+            var people = Object.keys(posters);
+            for (var i = 0; i < people.length; i++) {
+                if (posters[people[i]] > max) {
+                    max = posters[people[i]];
+                    most = people[i];
+                }
+            }
+            console.log(Object.keys(posters));
+            console.log(most);
+            var result = {"userID": most, "max": max};
+            response.success(result);
+        },
+        error: function(error) {
+            console.log("Error: " + error.code + " " + error.message);
         }
     });
 });
